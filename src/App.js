f44b24d0cb557d078477d52577fa8d58051a4769
kiran <gandikotakiran99@gmail.com>
2022-01-11 11:03:20 +0530

    reusable components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Button from './Button';
 import './style.css';
 
 const App=()=>{
     return(
         <div>
             <h1 style={{textAlign:"center"}}>Hello World!!</h1>
-            <button className='button'>Hello World</button>
+
+            <Button title="App Store" />
+            <Button title={"Play Store"} />
+            <Button />
+            {/* <button className='button'>Hello World</button> */}
         </div>
     )
 }
